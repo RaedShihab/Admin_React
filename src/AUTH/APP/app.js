c4350629616b2b24cef278cf958a5c4b6082076d
RaedShihab <raedshihab92@gmail.com>
2020-01-21 14:44:24 +0200

    auth has done

---
+++ src/AUTH/APP/app.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { history } from '../HELPERS/history';
+import { alertActions } from '../ACTIONS/alert.action';
+import { PrivateRoute } from '../COMPONENTS/privateRoutes';
+import { HomePage } from '../HOME_PAGE/homePage';
+import { LoginPage } from '../LOGIN_PAGE/loginPage';
+ 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const { dispatch } = this.props;
+        history.listen((location, action) => {
+            // clear alert on location change
+            dispatch(alertActions.clear());
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+            <div className="jumbotron">
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Router history={history}>
+                            <div>
+                                <PrivateRoute exact path="/" component={HomePage} />
+                                <Route path="/login" component={LoginPage} />
+                            </div>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { alert } = state;
+    return {
+        alert
+    };
+}
+
+const connectedApp = connect(mapStateToProps)(App);
+export default connectedApp;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Auth from './AUTH/APP/app'
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { store } from './AUTH/HELPERS/store';
+// import { configureFakeBackend } from './AUTH/HELPERS/fake.backend';
+// configureFakeBackend();
+ReactDOM.render(<Provider store={store}><Auth /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
