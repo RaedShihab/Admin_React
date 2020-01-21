0ea650eb2c760e79bf77633d5c0c1d030a005871
RaedShihab <raedshihab92@gmail.com>
2020-01-21 14:50:25 +0200

    Auth has done

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { store } from './AUTH/HELPERS/store';
 // import { configureFakeBackend } from './AUTH/HELPERS/fake.backend';
 // configureFakeBackend();
 ReactDOM.render(<Provider store={store}><Auth /></Provider>, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
